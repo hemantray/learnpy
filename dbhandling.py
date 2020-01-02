1cd919dd5742037899057755be5218cc74986190
Ray <hray1@ms.ds.uhc.com>
2020-01-03 05:05:19 +0530

    push dbhandling

---
+++ dbhandling.py
@@ -0,0 +1,17 @@
+import pyodbc 
+# Some other example server values are
+# server = 'localhost\sqlexpress' # for a named instance
+# server = 'myserver,port' # to specify an alternate port
+server = 'tcp:LHTU05CG84218GH' 
+database = 'mydb' 
+username = 'pyuser' 
+password = 'pyuser123' 
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
+
+#Sample select query
+cursor.execute("SELECT @@version;") 
+row = cursor.fetchone() 
+while row: 
+    print(row[0])
+    row = cursor.fetchone()
